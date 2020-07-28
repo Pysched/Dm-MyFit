f0a9e496ab374dd1807897a0f888f8abd36bb364
Pysched <murphyjdarren@gmail.com>
2020-07-28 02:07:12 +0000

    added more product details to cards

--- products/views.py
+++ products/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 from .models import Product
-from django.core.paginator import Paginator
 
 
 def all_products(request):
-    """ A view to return all products, searches and filtering of the  Product page """
+    """ A view to return all products etc """
 
     products = Product.objects.all()
     context = {
         'products': products,
     }
-    paginator = Paginator(products, 15)
-    page = request.GET.get('page')
-    
     return render(request, 'products/products.html', context)
