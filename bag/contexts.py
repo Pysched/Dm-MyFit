1eea3853580a03da474dc278072691ac27179fd7
Pysched <murphyjdarren@gmail.com>
2020-08-16 16:27:43 +0000

    updating login page

--- bag/contexts.py
+++ bag/contexts.py
@@ -7,7 +7,7 @@ from products.models import Product
 def bag_contents(request):
 
     bag_items = []
-    total = 0 
+    total = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
