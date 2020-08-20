a416a4b38210fb82d7608d08a720ef28ed286bae
Pysched <murphyjdarren@gmail.com>
2020-08-20 18:39:04 +0000

    updated shopping bag to work with adding and removing items and quantities, also fixed bugs, updated product deails page and added to styles.css

--- bag/contexts.py
+++ bag/contexts.py
@@ -27,14 +27,14 @@ def bag_contents(request):
                 total += quantity * product.price
                 product_count += quantity
                 bag_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-                'size': size,
-            })
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
@@ -46,6 +46,7 @@ def bag_contents(request):
         'bag_items': bag_items,
         'total': total,
         'product_count': product_count,
+        'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
