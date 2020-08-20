dc01a9553f9238b99ef67185bf5c812e06a865f4
Pysched <murphyjdarren@gmail.com>
2020-08-20 23:16:14 +0000

    added toasts to inculde, toast success, error amd warning, bug fixed views in bag app, total not working

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 
 def view_bag(request):
@@ -8,8 +10,8 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
-
+    """ Adding items to the bag, quantity management toast messages """
+    product = Product.objects.get(pl=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -30,6 +32,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag ')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
