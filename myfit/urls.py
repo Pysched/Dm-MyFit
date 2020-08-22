f3e86db74a0d44c62797d8fcf480135cd6e26a6b
Pysched <murphyjdarren@gmail.com>
2020-08-22 22:31:28 +0000

    added checkout app, templates, structure and css

--- myfit/urls.py
+++ myfit/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('nutrition/', include('nutrition.urls')),
     path('blog/', include('blog.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
