e4e720e4e30e56f726389855895d2ffce0139593
Pysched <murphyjdarren@gmail.com>
2020-08-30 17:42:42 +0000

    missing detail for herokuapp

--- myfit/settings.py
+++ myfit/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['myfit.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['myfit.herokuapp.com', 'localhost']
 
 
 # Application definition
