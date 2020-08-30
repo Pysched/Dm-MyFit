f27bb7242cf8ac57031e63ac519190f9313e12ba
Pysched <murphyjdarren@gmail.com>
2020-08-30 23:05:14 +0000

    flake 8 tidy up

--- profiles/migrations/0001_initial.py
+++ profiles/migrations/0001_initial.py
@@ -18,7 +18,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='UserProfile',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name='ID'
+                        )),
                 ('default_phone_number', models.CharField(blank=True, max_length=20, null=True)),
                 ('default_country', django_countries.fields.CountryField(blank=True, max_length=2, null=True)),
                 ('default_postcode', models.CharField(blank=True, max_length=20, null=True)),
