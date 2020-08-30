1aa310f72ecd87992f3f72d82d0efc42f32bbce9
Pysched <murphyjdarren@gmail.com>
2020-08-30 11:18:53 +0000

    reverted

--- myfit/urls.py
+++ myfit/urls.py
@@ -29,6 +29,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('contact/', include('contact.urls')),
-   
-   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
